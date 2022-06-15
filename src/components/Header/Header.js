1d48126b8c3acf50c8cb7ef6f08b0510a0ce6cce
mahdi bashiri <mahdi94250@gmail.com>
2022-06-15 19:54:00 +0430

    add feature hoverTraker in navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,21 +6,33 @@ import { BiBasket } from "react-icons/bi";
 import { BiMenu } from "react-icons/bi";
 import { FiZap } from "react-icons/fi";
 import { FiShoppingCart } from "react-icons/fi";
-
+import { useRef } from 'react';
 
 const Header = () => {
+
+  const navigatNavbar = useRef()
+
+  const trakerHoverHandler = (e) => {
+    let locationTarget = e.target.getBoundingClientRect();
+    navigatNavbar.current.style.left = (locationTarget.left - 40) + "px"
+    navigatNavbar.current.style.width = locationTarget.width + "px"
+  }
+  const outTrakerHandler = () => {
+    navigatNavbar.current.style.width = '0';
+  }
+
   return (
     <header className="w-fyll pt-4 bg-white border-b">
       <div className="container font-sans text-xl w-full flex items-center justify-center">
         <Link to={'/'} className="ml-6">
-          <img className="max-w-[115px]" src={"/image/logo/logo.svg"} alt="logo" />
+          <img className="max-w-[100px]" src={"/image/logo/logo.svg"} alt="logo" />
         </Link>
-        <div className="w-full h-full flex relative items-center justify-start">
+        <div className="relative w-full h-full flex items-center justify-start">
           <BiSearch className="absolute right-3 text-gray-400 text-2xl" />
           <input type={"text"} className="bg-gray-100 pl-4 text-gray-600 pr-12 py-3 w-full max-w-[600px] rounded-lg outline-none text-sm" placeholder="جسنوجو" />
         </div>
         <div className="flex items-center justify-center">
-          <Link to={'/'} className="after:content-['|'] after:h-full after:w-2 after:top-0.5 after:absolute after:-left-5 after:text-gray-300 after: relative flex items-center justify-center p-2 border rounded-lg border-gray-400">
+          <Link to={'/'} className="after:content-['|'] after:h-full after:w-2 after:top-0.5 after:absolute after:-left-7 after:text-gray-300 after: relative flex items-center justify-center py-2 px-6 border rounded-lg border-gray-400">
             <BiLogIn className="text-2xl" />
             <span className="text-sm whitespace-nowrap">ورود | ثبت نام</span>
           </Link>
@@ -29,32 +41,32 @@ const Header = () => {
           </Link>
         </div>
       </div>
-      <nav className="w-full flex justify-between items-center container">
-        <ul className="relative h-14 flex items-center justify-center text-sm font-sans text-gray-700">
-          <li className="peer bg-[linear-gradient(#DC2626,#DC2626)] bg-[length:0_2px] hover:bg-[length:100%_2px] hover:bg-right-bottom transition-[background-size] duration-300 bg-no-repeat bg-left-bottom overflow-hidden font-bold text-slate-800 after:content-[''] after:w-[1px] after:rounded-full after:bg-gray-300 after:h-[25px] after:absolute after:left-0 after:top-[50%] after:-translate-y-[50%] relative cursor-pointer h-full pl-4 flex items-center">
-            <BiMenu className="text-xl ml-1" />
+      <nav className="relative w-full flex justify-between items-center container">
+        <ul className="h-14 flex items-center justify-center text-sm font-sans text-gray-700">
+          <li onMouseOut={outTrakerHandler} onMouseOver={trakerHoverHandler} className="cursor-pointer overflow-hidden font-bold text-slate-800 after:content-[''] after:w-[1px] after:rounded-full after:bg-gray-300 after:h-[25px] after:absolute after:left-0 after:top-[50%] after:-translate-y-[50%] relative h-full pl-4 flex items-center">
+            <BiMenu className="text-xl ml-1 select-none" />
             دسته بندی کالا ها
           </li>
-          <li className="peer bg-[linear-gradient(#DC2626,#DC2626)] bg-[length:0_2px] hover:bg-[length:100%_2px] hover:bg-right-bottom transition-[background-size] duration-300 bg-no-repeat bg-left-bottom relative cursor-pointer overflow-hidden  h-full px-2 flex items-center">
+          <li onMouseOut={outTrakerHandler} onMouseOver={trakerHoverHandler} className="cursor-pointer overflow-hidden  h-full px-2 flex items-center">
             <BiBasket className="text-xl ml-1" />
             سوپرمارکت
           </li>
-          <li className="peer bg-[linear-gradient(#DC2626,#DC2626)] bg-[length:0_2px] hover:bg-[length:100%_2px] hover:bg-right-bottom transition-[background-size] duration-300 bg-no-repeat bg-left-bottom relative cursor-pointer overflow-hidden  h-full px-2 flex items-center">
+          <li onMouseOut={outTrakerHandler} onMouseOver={trakerHoverHandler} className="cursor-pointer overflow-hidden  h-full px-2 flex items-center">
             <FiZap className="text-xl ml-1" />
             پرفروش ترین
           </li>
-          <li className="peer bg-[linear-gradient(#DC2626,#DC2626)] bg-[length:0_2px] hover:bg-[length:100%_2px] hover:bg-right-bottom transition-[background-size] duration-300 bg-no-repeat bg-left-bottom relative cursor-pointer overflow-hidden  h-full px-2 flex items-center">
+          <li onMouseOut={outTrakerHandler} onMouseOver={trakerHoverHandler} className="cursor-pointer overflow-hidden  h-full px-2 flex items-center">
             <BiMapAlt className="text-xl ml-1" />
             تخفیف ها
           </li>
-          <li className="peer bg-[linear-gradient(#DC2626,#DC2626)] bg-[length:0_2px] hover:bg-[length:100%_2px] hover:bg-right-bottom transition-[background-size] duration-300 bg-no-repeat bg-left-bottom overflow-hidden after:content-[''] after:w-[1px] after:rounded-full after:bg-gray-300 after:h-[25px] after:absolute after:left-0 after:top-[50%] after:-translate-y-[50%] relative relative cursor-pointer  h-full pl-4 flex items-center">
+          <li onMouseOut={outTrakerHandler} onMouseOver={trakerHoverHandler} className="cursor-pointer overflow-hidden relative after:content-[''] after:w-[1px] after:rounded-full after:bg-gray-300 after:h-[25px] after:absolute after:left-0 after:top-[50%] after:-translate-y-[50%] h-full pl-4 flex items-center">
             <BiMapAlt className="text-xl ml-1" />
             شگفت انگیزها
           </li>
-          <li className="peer bg-[linear-gradient(#DC2626,#DC2626)] bg-[length:0_2px] hover:bg-[length:100%_2px] hover:bg-right-bottom transition-[background-size] duration-300 bg-no-repeat bg-left-bottom relative cursor-pointer overflow-hidden  h-full px-2 flex items-center">
+          <li onMouseOut={outTrakerHandler} onMouseOver={trakerHoverHandler} className="cursor-pointer overflow-hidden  h-full px-2 flex items-center">
             سوالی دارید؟
           </li>
-          <li className="peer bg-[linear-gradient(#DC2626,#DC2626)] bg-[length:0_2px] hover:bg-[length:100%_2px] hover:bg-right-bottom transition-[background-size] duration-300 bg-no-repeat bg-left-bottom cursor-pointer overflow-hidden  h-full px-2 flex items-center">
+          <li onMouseOut={outTrakerHandler} onMouseOver={trakerHoverHandler} className="cursor-pointer overflow-hidden  h-full px-2 flex items-center">
             فروشنده شوید!
           </li>
         </ul>
@@ -64,6 +76,7 @@ const Header = () => {
             مطفا شهر خود را انتخاب کنید
           </li>
         </ul>
+        <span ref={navigatNavbar} className="duration-200 transition-all block h-[3px] rounded-full bg-red-600 absolute bottom-0"></span>
       </nav>
     </header>
   );
