7527755d98a7cccad2b1365266accaefe7e1baf2
mahdi bashiri <mahdi94250@gmail.com>
2022-06-18 16:26:03 +0430

    add Page Login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './page/Home';
+import LoginPage from './page/LoginPage';
+import Layout from "./layout/Layout"
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
+      <Route path="/login" element={<LoginPage />} />
     </Routes>
   );
 }
---
+++ src/components/Login/Login.js
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+
+const Login = () => {
+  return (
+    <section className="fixed w-screen h-screen z-50 top-0 right-0 bg-white flex flex-col items-center justify-center">
+      <form className="w-full max-w-[400px] px-8 md:border rounded-lg flex flex-col items-center justify-center">
+        <div className="w-full max-w-[150px]">
+          <img className="w-full object-cover py-8" src="/image/logo/logo.svg" alt="logoSite" />
+        </div>
+        <h2 className="font-sans text-2xl text-bold w-full text-right mb-9">ورود | ثبت نام</h2>
+        <div className="gap-y-1.5 font-sans text-sm text-slate-600 w-full flex flex-col items-start justify-center">
+          <span>سلام!</span>
+          <p>لطفا شماره موبایل یا ایمیل خود را وارد کنید</p>
+        </div>
+        <input type={"text"} className="outline-none border rounded-lg w-full p-3 mt-5 mb-8" />
+        <button className="w-full p-4 bg-red-500 text-center rounded-lg font-sans text-white">ورود</button>
+        <p className="font-sans whitespace-pre-wrap text-sm text-slate-800 flex items-center justify-center mt-6 mb-10">
+          ورود شما به معنای پذیرش
+          <Link to={"#"} className="block mx-2 whitespace-nowrap font-sans text-[12px] text-blue-700">قوانین حریم خصوصی</Link>
+          است
+        </p>
+      </form>
+    </section>
+  );
+}
+
+export default Login;
